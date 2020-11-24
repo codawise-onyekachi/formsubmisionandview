dc17f86c4ff6b9cd34dba682beab284eef183f41
Onyekachi Uzoma <onyekachihopeuzoma@gmail.com>
2020-11-24 09:03:06 +0100

    formsubmissiomandretrieval version2

--- src/main/java/com/example/form/FormController.java
+++ src/main/java/com/example/form/FormController.java
@@ -1,13 +1,19 @@
 package com.example.form;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class FormController {
+
+    @Autowired
+    CustomerRepository customerRepository;
 
     @RequestMapping("/home")
     public String educate(){
@@ -15,15 +21,26 @@ public class FormController {
         return "index.html";
     }
 
-    @PostMapping("/details")
-    public String viewdetails(@RequestParam("cid") Long cid,
-                              @RequestParam("cname") String cname,
-                              @RequestParam("cemail") String cemail,
-                              ModelMap modelMap){
-        modelMap.put("cid", cid);
-        modelMap.put("cname", cname);
-        modelMap.put("cemail", cemail);
+    @RequestMapping("/details")
+    public String details(Customers customers){
+        //return "educate.jsp";
+        customerRepository.save(customers);
+        return "index.html";
+    }
 
+    @RequestMapping("/getdetails")
+    public String getdetails(){
+        //return "educate.jsp";
         return "viewCustomerPage.html";
+    }
+
+
+    @PostMapping("/getdetails")
+    public ModelAndView getdetails(@RequestParam Long cid){
+
+        ModelAndView mv = new ModelAndView("Retrieve.html");
+        Customers customers = customerRepository.findById(cid).orElse(null);
+        mv.addObject(customers);
+        return mv;
     }
 }
